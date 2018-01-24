459eeaff85b68a24862bae57adef896a91898300
Francisco Gallardo <Francisco Gallardo@DESKTOP-KGEN2DA>
2018-01-24 01:08:25 -0300

    continuar con clase 51.

--- app/ValidadorEntrada.inc.php
+++ app/ValidadorEntrada.inc.php
@@ -33,21 +33,99 @@ class ValidadorEntrada {
         }
     }
 
-    public function validar_titulo($conexion, $titulo) {
+    private function validar_titulo($conexion, $titulo) {
         if (!$this->variable_iniciada($titulo)) {
             return "debes escribir un titulo";
         } else {
             $this->titulo = $titulo;
         }
-        
-        if(strlen($titulo) > 255){
+
+        if (strlen($titulo) > 255) {
             return "El titulo no puede ocupar mas de 255 caracteres";
         }
-        
-        if(RepositorioEntrada::titulo_existe($conexion, $titulo)){
+
+        if (RepositorioEntrada::titulo_existe($conexion, $titulo)) {
             return "Ya existe una entrada con este titulo, por favor ingresa otro valido";
         }
-        
+    }
+
+    private function validar_url($conexion, $url) {
+        if (!$this->variable_inicada($url)) {
+            return "Debes insertar una URL";
+        } else {
+            $this->url = $url;
+        }
+        if (strlen($url) != strlen(trim($url))) {
+            return "La URL no puede contener espacios";
+        }
+
+        if (RepositorioEntrada::url_existe($conexion, url)) {
+            return "Ya existe otro articulo con la misma URL, elige una diferente.";
+        }
+    }
+
+    private function validar_texto($conexion, $texto) {
+        if (!$this->variable_iniciada($texto)) {
+            return "El contenido no puede estar vacío";
+        } else {
+            $this->texto = $texto;
+        }
+    }
+
+    function getTitulo() {
+        return $this->titulo;
+    }
+
+    function getUrl() {
+        return $this->url;
+    }
+
+    function getTexto() {
+        return $this->texto;
+    }
+
+    public function mostrar_titulo() {
+        if (!$this->titulo != "") {
+            echo 'value= "' . $this->titulo . '"';
+        }
+    }
+
+    public function mostrar_url() {
+        if (!$this->titulo != "") {
+            echo 'value= "' . $this->url . '"';
+        }
+    }
+
+    public function mostrar_texto() {
+        if ($this->texto != "" & strlen(trim($this->texto)) > 0) {
+            echo $this->texto;
+        }
+    }
+
+    public function mostrar_error_titulo() {
+        if (!$this->error_texto != "") {
+            echo $this->aviso_inico . $error_titulo . $this->aviso_cierre;
+        }
+    }
+
+    public function mostrar_error_texto() {
+        if (!$this->error_texto != "") {
+            echo $this->aviso_inico . $error_titulo . $this->aviso_cierre;
+        }
+    }
+
+    public function mostrar_error_url() {
+        if (!$this->error_texto != "") {
+            echo $this->aviso_inico . $error_titulo . $this->aviso_cierre;
+        }
+    }
+
+    public function entrada_valida() {
+        if ($this->error_titulo == "" && error_url == "" && $this->error_texto == "") {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- vistas/nueva-entrda.php
+++ vistas/nueva-entrda.php
@@ -1,11 +1,40 @@
 <?php
 include_once 'app/config.inc.php';
 include_once 'app/Conexion.inc.php';
-include_once 'app/RepositorioUsuario.inc.php';
-include_once 'app/Usuario.inc.php';
+include_once 'app/RepositorioEntrada.inc.php';
+include_once 'app/Entrada.inc.php';
+
+include_once 'app/ValidadorEntrada.inc.php';
+include_once 'app/ControlSesion.inc.php';
+include_once 'app/Redireccion.inc.php';
 $titulo = "Nueva Entrada";
 include_once 'plantillas/documento-declaracion.inc.php';
 include_once 'plantillas/navbar.inc.php';
+
+$entrada_publica = 0;
+if (isset($_POST['guardar'])) {
+    Conexion::abrir_conexion();
+
+    $validador = new ValidadorEntrada($_POST['titulo'], $_POST['url'], htmospecialchars($_POST['texto']), Conexion::obtener_conexion());
+    if (isset($_POST['publicar']) && $_POST['publicar'] == 'si') {
+        $entrada_publica = 1;
+    }
+
+    if ($validador->entrada_valida()) {
+        if (ControlSesion::sesion_inicada()) {
+            $entrada = new Entrada('', $_SESSION['id_usuario'], $validador->getUrl(), $validador->getTitulo(), $validador->getTexto(), '', $entrada_publica);
+            $entrada_insertada = RepositorioEntrada::insertar_entrada(Conexion::obtener_conexion(), $entrada);
+            if ($entrada_insertada) {
+                Redireccion:redirigir(RUTA_GESTOR_ENTRADAS);
+                
+            }else{
+                Redireccion::redirigir(RUTA_LOGIN);
+            }
+        }
+        Conexion::cerrar_conexion();
+    }
+    
+}
 ?>
 <div class="container">
     <div class="jumbotron">
